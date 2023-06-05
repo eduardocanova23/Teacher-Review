8fb6fa1513f170d422fd93dbb330442bcd5abfb6
Yasmin Forestti <87791176+YasminForestti@users.noreply.github.com>
2023-06-05 18:59:58 -0300

    add sideDrower

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,29 @@
 import './App.css';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import TemporaryDrawer from './Components/LeftDrower';
 
 function App() {
+
   return (
+    <Box>
+      <TemporaryDrawer/>
     <div className="App">
       <header className="App-header">
-        Teste6
+        Teste7
       </header>
     </div>
+    
+    </Box>
   );
 }
 
