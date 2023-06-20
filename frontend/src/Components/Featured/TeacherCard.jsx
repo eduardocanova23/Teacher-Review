7da316e9ffd68be30eae375db608ba33ff7f51f7
MariaLuizaCw <marialuizacw@gmail.com>
2023-06-19 22:46:47 -0300

    implement routes

--- frontend/src/Components/Featured/TeacherCard.jsx
+++ frontend/src/Components/Featured/TeacherCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../../css/TeacherCard.css';
-
+import { Link } from "react-router-dom";
 
 const TeacherCard = ({ teacher, image }) => {
   const handleClick = () => {
@@ -10,12 +10,16 @@ const TeacherCard = ({ teacher, image }) => {
 
 
   return (
-    <div className="card">
-      <img src={image} alt="Teacher" className="card-image" />
-      <div >
-        <h5>{teacher.label}</h5>
-      </div>
-    </div>
+    <Link to={`/professors/${teacher.id}`} style={{ textDecoration: 'none' }}>
+       <div className="card">
+       
+       <img src={image} alt="Teacher" className="card-image" />
+       <div >
+         <h5>{teacher.label}</h5>
+       </div>
+     </div>
+    </Link>
+   
   );
 };
 
---
+++ frontend/src/Components/Professor/ProfessorDetails.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+import {useParams} from "react-router-dom"
+import { useNavigate} from "react-router-dom";
+import IconButton from '@mui/material/IconButton';
+import ArrowBack from '@mui/icons-material/ArrowBack';
+
+function ProfessorDetail() {
+    const {professorId} = useParams()
+    let navigate = useNavigate();
+    return (
+        <div>
+            <div style={{position:'absolute', margin: 20}}>
+                <IconButton onClick={() => navigate(-1)} aria-label="delete" size="small">
+                <ArrowBack fontSize="small" />
+                Voltar
+                </IconButton>
+            </div>
+            <div>
+                <h1>Teste {professorId}</h1>
+            </div>
+        </div>
+    )
+}
+
+export default ProfessorDetail
