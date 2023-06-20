6582e38b43c12b533432f6fb6e0d3eea90d407c2
MariaLuizaCw <marialuizacw@gmail.com>
2023-06-19 23:36:34 -0300

    add recent review

--- frontend/src/Components/PopularReviews/PopularReviews.js
+++ frontend/src/Components/PopularReviews/PopularReviews.js
@@ -37,26 +37,30 @@ export default function PopularReviews(props) {
       <Box
         sx={{
           display: 'flex',
+          borderRadius: 4,
+          padding: 5,
           flexDirection: 'column',
           alignItems: 'left',
-          width: 700,
-          padding: '10px',
-          margin: '17px',
           backgroundColor: '#FFFFFF',
-          borderRadius: '17px',
-          paddingTop: '5px'
+          margin: 2,
+          width: '60%'
         }}
       >
-        <span style={{ color: '#282828', fontSize: '13px'}}>{(props.review != null) ? props.review.professor_name : ''} - {(props.review != null) ? props.review.subject_name : ''}</span>
-        <p>
-          <span style={{ color: '#282828', fontSize: '17px'}}>{(props.review != null) ? props.review.description : ''}</span>
-        </p>
-        <div style={{ borderBottom: '4px solid #40A737', width: '100%', marginTop: '-10px' }}></div>
+           <span style={{ color: '#282828', fontSize: '13px'}}>{(props.review != null) ? props.review.professor_name : ''} - {(props.review != null) ? props.review.subject_name : ''}</span>
+          <div  style={{display: 'flex', flexDirection: 'row', alignItems: 'flex-end'}}>
         <span style={{ color: '#282828', fontSize: '17px', fontWeight: 'bold'}}>Didática: {(props.review != null) ? getStars(props.review.metric1) : ''}</span>
         <span style={{ color: '#282828', fontSize: '17px', fontWeight: 'bold'}}>Organização: {(props.review != null) ? getStars(props.review.metric2) : ''}</span>
         <span style={{ color: '#282828', fontSize: '17px', fontWeight: 'bold'}}>Empatia: {(props.review != null) ? getStars(props.review.metric3) : ''}</span>
         <span style={{ color: '#282828', fontSize: '17px', fontWeight: 'bold'}}>Pontualidade: {(props.review != null) ? getStars(props.review.metric4) : ''}</span>
         <span style={{ color: '#282828', fontSize: '17px', fontWeight: 'bold'}}>Mamata: {(props.review != null) ? getStars(props.review.metric5) : ''}</span>
+        </div>
+       
+        <div style={{ borderBottom: '4px solid #40A737', width: '100%', marginTop: '-10px' }}></div>
+      
+     
+        
+        <span style={{ color: '#282828', fontSize: '17px'}}>{(props.review != null) ? props.review.description : ''}</span>
+       
       </Box>
     </>
   );
