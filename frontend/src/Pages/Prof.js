653ee21eba1147e8a548686d144eb21b1bb3f91a
Yasmin Forestti <87791176+YasminForestti@users.noreply.github.com>
2023-06-19 22:58:18 -0300

    Add button

--- frontend/src/Pages/Prof.js
+++ frontend/src/Pages/Prof.js
@@ -2,7 +2,10 @@ import React, { useState, useEffect } from 'react';
 import DescriptionMap from '../Components/Professor/DescriptionMap'
 import {useDescription} from '../Components/Professor/hooks/useDescription'
 import Box from '@mui/material/Box';
-
+import {useParams} from "react-router-dom"
+import { navigate,useNavigate} from "react-router-dom";
+import IconButton from '@mui/material/IconButton';
+import ArrowBack from '@mui/icons-material/ArrowBack';
 import ProfessorSubjectContainer from '../Components/ProfessorSubject/ProfessorSubjectContainer'
 
 export default function ProfComponent() {
@@ -15,8 +18,18 @@ export default function ProfComponent() {
   
   return (
     <Box>
+<IconButton onClick={() => navigate(-1)} aria-label="delete" size="small">
+                <ArrowBack fontSize="small" />
+                Voltar
+                </IconButton>
+<div style={{position:'absolute', margin: 20}}>
+                <IconButton onClick={() => navigate(-1)} aria-label="delete" size="small">
+                <ArrowBack fontSize="small" />
+                Voltar
+                </IconButton>
+            </div>
+      <ProfessorScoreContainer/>
       <ProfessorSubjectContainer id = {id} reviewList = {reviewList}/>
-
     </Box>
   );
 }
